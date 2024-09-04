6793347254ba7f50867e9c6fef898a305e30bd1f
NavaneetSingh <154007236+Ns-Dev64@users.noreply.github.com>
2024-09-04 10:28:58 +0530

    Update server.js

--- week-1-tasks/quoteBackend/server.js
+++ week-1-tasks/quoteBackend/server.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv').config() //to use the stuff from .env file
 const ConnectDb=require('./db')
 const cors=require('cors')
 app.use(cors())
+app.use(cors({
+    origin: 'https://66d6b9dcafb800d5b51e3b05--lively-gelato-6b965e.netlify.app/' // Replace with your frontend domain
+}));
 const port = process.env.PORT;
 app.use(express.json())
 //Listening on the server at a port
